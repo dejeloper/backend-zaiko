5d9d75a1673a01b99290110dccfa94e72da46415
Jhonatan Guerrero <jhonatanguerrero@outlook.com>
2023-07-03 21:46:28 -0500

    Add express and config first server

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-console.log("Hola");
+const express = require("express");
+
+const app = express();
+const port = 3000;
+
+app.get("/", (req, res) => {
+  res.send("Hola desde mi servidor en express");
+});
+
+app.listen(port, () => {
+  console.log("Corriendo en el puerto: " + port);
+});
