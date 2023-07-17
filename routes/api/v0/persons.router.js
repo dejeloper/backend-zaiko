e3fa689e1733c02696f28ba296b6aafc77167865
Jhonatan Guerrero <jhonatanguerrero@outlook.com>
2023-07-16 22:49:53 -0500

    Add CRUD Routes with Schemas and Services

--- routes/api/v0/persons.router.js
+++ routes/api/v0/persons.router.js
@@ -1,13 +1,12 @@
 const express = require("express");
 
 const PersonsService = require("../../../services/v0/persons.service");
-// const validatorHandler = require("../../../middlewares/validator.handler");
-
-// const {
-//   createProductSchema,
-//   updateProductSchema,
-//   getProductSchema,
-// } = require("../../../schemas/product.schema");
+const validatorHandler = require("../../../middlewares/validator.handler");
+const {
+  createPersonsSchema,
+  getPersonSchema,
+  updatePersonSchema,
+} = require("../../../schemas/persons.schema");
 
 const router = express.Router();
 const service = new PersonsService();
@@ -20,5 +19,59 @@ router.get("/", async (req, res, next) => {
     next(error);
   }
 });
+
+router.get(
+  "/:Id",
+  validatorHandler(getPersonSchema, "params"),
+  async (req, res, next) => {
+    try {
+      const { Id } = req.params;
+      const person = await service.findOne(Id);
+      res.json(person);
+    } catch (error) {
+      next(error);
+    }
+  }
+);
+
+router.post(
+  "/",
+  validatorHandler(createPersonsSchema, "body"),
+  async (req, res) => {
+    const body = req.body;
+    const newProduct = await service.create(body);
+    res.status(201).json(newProduct);
+  }
+);
+
+router.patch(
+  "/:Id",
+  validatorHandler(getPersonSchema, "params"),
+  validatorHandler(updatePersonSchema, "body"),
+  async (req, res, next) => {
+    try {
+      const { Id } = req.params;
+      const body = req.body;
+      const person = await service.update(Id, body);
+      res.json(person);
+    } catch (error) {
+      next(error);
+    }
+  }
+);
+
+router.delete(
+  "/:Id",
+  validatorHandler(getPersonSchema, "params"),
+  async (req, res, next) => {
+    try {
+      const { Id } = req.params;
+      const person = await service.delete(Id);
+      res.json(person);
+    } catch (error) {
+      next(error);
+    }
+  }
+);
 
 module.exports = router;
